798c1305c3613cd4e51975aff33e5669b99ad3fa
spencer18001 <spencer18001@gmail.com>
2025-06-16 15:21:28 +0000

    [新增] 前端初版

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # LearningBar
+
+## front
+安裝相依套件
+```shell
+npm install
+```
+
+啟動開發伺服器
+```shell
+npm run dev
+```
+瀏覽器開啟：http://localhost:5173
---
+++ front/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">歡迎來到 學吧！</h1>
+    </div>
+  );
+};
+
+export default Home;
